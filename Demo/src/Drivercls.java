8c3ea688dfcdadeba2905d5db8437c89490007d5
NATRAJ <NATRAJ@LAPTOP-5O77A6PH>
2022-12-23 14:47:22 +0530

    New project.

---
+++ Demo/src/Drivercls.java
@@ -0,0 +1,11 @@
+
+public class Drivercls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Objectcls o;
+         o=new Objectcls();
+         o.display();
+	}
+
+}
